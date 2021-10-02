e1db231ecf83c6dcde3e71e7f98c3da2546781e5
wilfrido bonett <wbonett10@gmail.com>
2021-10-02 18:20:09 -0500

    Toast service created

--- src/app/modules/rental/rental.module.ts
+++ src/app/modules/rental/rental.module.ts
@@ -2,21 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { NgxMaskModule, IConfig } from 'ngx-mask';
 import { RentalPageRoutingModule } from './rental-routing.module';
 import { RentalPage } from './rental.page';
 import { ModalOptionsComponent } from 'src/app/components/modal-options/modal-options.component';
-
-export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
+import { RentalService } from 'src/app/services/rental.service';
+import { ToastService } from 'src/app/services/toast.service';
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    NgxMaskModule.forRoot(),
     FormsModule,
     IonicModule,
     RentalPageRoutingModule,
   ],
   declarations: [RentalPage, ModalOptionsComponent],
+  providers: [RentalService, ToastService],
 })
 export class RentalPageModule {}
--- src/app/modules/rental/rental.page.ts
+++ src/app/modules/rental/rental.page.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { ModalController, NavController } from '@ionic/angular';
-import * as moment from 'moment';
+import { ActivatedRoute } from '@angular/router';
+import {
+  AlertController,
+  ModalController,
+  NavController,
+} from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { ModalOptionsComponent } from 'src/app/components/modal-options/modal-options.component';
 import { STATIC } from 'src/app/data';
 import { IInventory } from 'src/app/models/inventory.interface';
 import { InventoryService } from 'src/app/services/inventory.service';
+import { RentalService } from 'src/app/services/rental.service';
 import { StorageService } from 'src/app/services/storage.service';
+import { ToastService } from 'src/app/services/toast.service';
 @Component({
   selector: 'app-rental-page',
   templateUrl: './rental.page.html',
@@ -18,21 +24,23 @@ export class RentalPage implements OnInit {
   public validation_messages = {
     bike: [],
     rentDate: [],
-    fullname: [],
     email: [],
     phone: [],
   };
   public initDate: string = new Date().toISOString();
   public inventoryList: Array<IInventory> = [];
   private inventorySubscription$: Subscription = null;
-  private bikeSelected: object = null;
+  private bikeSelected: any;
 
   constructor(
     private formBuilder: FormBuilder,
     private navCtrl: NavController,
+    private rentalService: RentalService,
     private storageService: StorageService,
     private inventoryService: InventoryService,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private route: ActivatedRoute,
+    private toastService: ToastService
   ) {
     this.inventorySubscription$ = this.inventoryService.inventory$.subscribe(
       (items) => {
@@ -43,12 +51,19 @@ export class RentalPage implements OnInit {
 
   ngOnInit() {
     this.initializeForm();
-
     this.getItemSelected();
   }
 
+  private validateBikeSelected() {
+    this.route.params.subscribe((params: object) => {
+      const id = params['id'];
+      this.updateRentFormValues(id);
+    });
+  }
+
   async getItemSelected() {
-    this.bikeSelected = await this.storageService.get('bike_details');
+    this.bikeSelected = await this.storageService.get('bike_selected');
+    this.validateBikeSelected();
   }
 
   private initializeForm(): void {
@@ -58,13 +73,12 @@ export class RentalPage implements OnInit {
         PHONE_REGEXP: regexPhone,
         FULLNAME_REGEXP: regexFullName,
       },
-      ERROR_MESSAGE: { BIKE, DATE, EMAIL, PHONE, FULLNAME },
+      ERROR_MESSAGE: { BIKE, DATE, EMAIL, PHONE },
     } = STATIC;
 
     this.validation_messages = {
       bike: BIKE,
       rentDate: DATE,
-      fullname: FULLNAME,
       email: EMAIL,
       phone: PHONE,
     };
@@ -72,15 +86,6 @@ export class RentalPage implements OnInit {
     this.rentalForm = this.formBuilder.group({
       bike: ['', Validators.compose([Validators.required])],
       rentDate: ['', Validators.compose([Validators.required])],
-      fullname: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.pattern(regexFullName),
-          Validators.minLength(10),
-          Validators.maxLength(50),
-        ]),
-      ],
       phone: [
         '',
         Validators.compose([
@@ -103,6 +108,12 @@ export class RentalPage implements OnInit {
     this.inventoryService.getInventory();
   }
 
+  private updateRentFormValues(id: string): void {
+    const getTitle = id !== '0' ? this.bikeSelected.title : '';
+
+    this.rentalForm.get('bike').setValue(getTitle);
+  }
+
   public async selectBikeInformation() {
     const modal = await this.modalCtrl.create({
       component: ModalOptionsComponent,
@@ -117,13 +128,15 @@ export class RentalPage implements OnInit {
       },
     });
     modal.onDidDismiss().then((res: any) => {
-      const {
-        data: { id },
-      } = res;
+      const { id } = res && res.data;
       if (id !== '') {
         const bike = this.inventoryList.filter((item) => item.id === id);
         this.rentalForm.get('bike').setValue(bike[bike.length - 1].title);
-        this.bikeSelected = res.data;
+        this.bikeSelected = this.findItemById(id);
+      } else {
+        this.toastService.presentErrorToast(
+          'Es necesario que seleccione una bicicleta para continuar'
+        );
       }
     });
     await modal.present();
@@ -133,37 +146,52 @@ export class RentalPage implements OnInit {
     return this.rentalForm.controls;
   }
 
-  public saveForm(payload: any): void {
-    /*   const payload = data,
-          buttons = [
-        {
-          cssClass: 'secondaryClass',
-          text: 'Reenviar email link',
-          handler: () => {
-            this._authService.sendEmailLink();
-            setTimeout(() => {
-              this._alertService.createAlertPayload('Mensaje Enviado!', 'iconSuccess', 'Por favor revisa tu bandeja de entrada para activar tu cuenta', '');
-            }, 1500);
-          }
-        },
-        {
-          cssClass: 'primaryClass',
-          text: 'Aceptar',
-          handler: () => { }
-        }
-      ];
+  private findItemById(id) {
+    const result = this.inventoryList.filter((item) => item.id === id);
+    return result[0];
+  }
 
-    this._authService.signIn(payload).then(
-      (result) => {
-        const user = result && result.user;
-        if (user && user.emailVerified) {
-          this._navCtrl.navigateRoot(this.nextScreen);
-        } else {
-          this._alertService.createAlertPayload('Ops!', 'iconError', 'Debes verificar tu cuenta para iniciar sesión', buttons);
-        }
-      }).catch((error) => {
-        this._alertService.createAlertPayload('Ops!', 'iconError', 'No es posible iniciar sesión, verifica tus credenciales', '');
-      });; */
+  public submitRentalForm(form: any): void {
+    const { bike, email, phone, rentDate } = form;
+    const {
+      category,
+      categoryId,
+      color,
+      description,
+      id,
+      key,
+      location,
+      size,
+      thumbnail,
+      title,
+    } = this.bikeSelected;
+
+    const payload = {
+      bike,
+      email,
+      phone,
+      rentDate,
+      category,
+      categoryId,
+      color,
+      description,
+      id,
+      key,
+      location,
+      size,
+      thumbnail,
+      title,
+    };
+
+    this.rentalService.createNewRental(payload).then((isRegistered) => {
+      if (isRegistered) {
+        this.toastService.presentSuccessToast(
+          `Se ha creado su renta exitosamente para el dia ${rentDate}.`
+        );
+        this.rentalForm.reset();
+        this.navCtrl.navigateRoot('/inventory');
+      }
+    });
   }
 
   ngOnDestroy() {
